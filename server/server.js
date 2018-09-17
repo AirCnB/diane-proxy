68aed964533c4b84d44593fdded7386e144a6d15
Dianewh <32206840+dianewh@users.noreply.github.com>
2018-09-17 16:34:58 -0700

    Update links

--- server/server.js
+++ server/server.js
@@ -12,21 +12,28 @@ app.listen(3000, () => console.log('Proxy-server is listening on port 3000'));
 
 app.get('/', (req, res) => res.send('reviews'));
 
-
 //re-route photo carousel get and post requests
 app.get('/api/listings/:id/photos', (request, res) => {
   let id = Number(request.params.id);
-  axios.get(`http://ec2-13-59-102-97.us-east-2.compute.amazonaws.com/api/listings/${id}/photos`)
+  axios.get(`http://ec2-18-188-253-85.us-east-2.compute.amazonaws.com/api/listings/${id}/photos`)
     .then(response => {res.status(200).send(response.data)})
     .catch( err => {res.status(500).send(err)});
 });
 
 app.post('/api/listings/:id/saved', (req, res) => {
   const request = Object.keys(req.body)[0];
-  axios.post(`http://ec2-13-59-102-97.us-east-2.compute.amazonaws.com/saved`, request)
+  axios.post(`http://ec2-18-188-253-85.us-east-2.compute.amazonaws.com/saved`, request)
     .then( () => {res.status(200).send()})
     .catch(err => {res.status(500).send(err)});
  });
+
+//re-route bookings get and post requests
+app.get('/api/listings/:id/bookings', (request, res) => {
+  let id = Number(request.params.id);
+  axios.get(`http://ec2-34-207-95-35.compute-1.amazonaws.com/api/listings/${id}/bookings`)
+    .then(response => {res.status(200).send(response.data)})
+    .catch(err => {res.status(500).send(err)});
+});
 
  //re-route house description get request
 app.get('/api/house/:id', (req, res) => {
@@ -36,7 +43,7 @@ app.get('/api/house/:id', (req, res) => {
     .catch( err => {res.status(500).send(err)})
 });
 
- //re-route reviews get and post requests
+//re-route reviews get and post requests
 app.get('/:id/reviews', (request, res) => {
   let id = Number(request.params.id);
   axios.get(`http://ec2-52-87-204-118.compute-1.amazonaws.com/${id}/reviews`)
@@ -52,11 +59,3 @@ app.post('/:id/reviews/query=:searchTerm', (req, res) => {
     .then(response => {res.status(200).send(response.data)})
     .catch(err => {res.status(404).send(err)});
  });
-
- //re-route bookings get and post requests
- app.get('/api/listings/:id/bookings', (request, res) => {
-  let id = Number(request.params.id);
-  axios.get(`http://ec2-34-203-218-92.compute-1.amazonaws.com/api/listings/${id}/bookings`)
-    .then(response => {res.status(200).send(response.data)})
-    .catch(err => {res.status(500).send(err)});
-});
